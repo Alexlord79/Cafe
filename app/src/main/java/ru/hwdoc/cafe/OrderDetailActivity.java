aa1ca20d533af3e29dccd22975432a0924a465cb
Alexlord79 <oranskiy79@yandex.ru>
2026-03-01 12:36:08 +0300

    Cafe. Логика третьего экрана

--- app/src/main/java/ru/hwdoc/cafe/OrderDetailActivity.java
+++ app/src/main/java/ru/hwdoc/cafe/OrderDetailActivity.java
@@ -3,6 +3,7 @@ package ru.hwdoc.cafe;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +18,11 @@ public class OrderDetailActivity extends AppCompatActivity {
     private static final String EXTRA_DRINK_TYPE = "drinkType";
     private static final String EXTRA_ADDITIVES = "additives";
 
+    private TextView textViewName;
+    private TextView textViewDrink;
+    private TextView textViewDrinkType;
+    private TextView textViewAdditives;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +33,19 @@ public class OrderDetailActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
+        Intent intent = getIntent();
+        textViewName.setText(intent.getStringExtra(EXTRA_USER_NAME));
+        textViewDrink.setText(intent.getStringExtra(EXTRA_DRINK));
+        textViewDrinkType.setText(intent.getStringExtra(EXTRA_DRINK_TYPE));
+        textViewAdditives.setText(intent.getStringExtra(EXTRA_ADDITIVES));
+    }
+
+    private void initViews() {
+        textViewName = findViewById(R.id.textViewName);
+        textViewDrink = findViewById(R.id.textViewDrink);
+        textViewDrinkType = findViewById(R.id.textViewDrinkType);
+        textViewAdditives = findViewById(R.id.textViewAdditives);
     }
 
     public static Intent newIntent(
